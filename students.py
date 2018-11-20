61db705d615bb630fd89c8a8acd2d84ca00c343a
srikarn <srikarnanduri628@gmail.com>
2018-11-20 15:29:19 +0530

    Initial commit

---
+++ students.py
@@ -0,0 +1,15 @@
+students = {
+    "Alice": {"id":"ID0001", "age":26, "grade":"A"},
+    "Bob": {"id":"ID0002", "age":27, "grade":"B"},
+    "Clare": ["ID0003", 17, "C"],
+    "Dan": ["ID0004", 21, "D"],
+    "Emma": ["ID0005", 22, "E"]
+    }
+
+print(students["Alice"])
+print(students["Clare"][0])
+print(students["Alice"]["age"])
+
+
+
+
